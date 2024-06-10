dde6236ac4dc5593149bdc8e196fb6c79470d613
rlatneorp <rlatneorp@gmail.com>
2024-06-10 12:38:03 +0900

    헤더 커밋

--- skeletone-app/src/components/Header.vue
+++ skeletone-app/src/components/Header.vue
@@ -3,18 +3,38 @@
     <h1 class="headerName">가계부</h1>
     <div class="headerRight">
       <div class="input_date_box">
-        <input type="date" />
+        <input
+          type="month"
+          data-placeholder="날짜 선택"
+          required
+          aria-required="true"
+        />
       </div>
-      <span class="material-symbols-outlined"> account_circle </span>
+      <div class="profile-wrapper" @click="toggleDropdown">
+        <span class="profile material-symbols-outlined"> account_circle </span>
+        <div class="dropdown" v-if="isDropdownVisible">
+          <ul>
+            <li>
+              <h3 class="dropdown-name">김용우 님{{ name }}</h3>
+            </li>
+            <li>
+              <span class="set material-symbols-outlined">설정 settings </span>
+            </li>
+          </ul>
+        </div>
+      </div>
     </div>
   </div>
 </template>
-<script setup>
-// composition api에서 값형 데이터를 반응형으로 만들때 ref 사용
-import { ref } from 'vue';
-const isNavShow = ref(false);
-</script>
 
+<script setup>
+import { ref } from 'vue';
+const isDropdownVisible = ref(false);
+
+const toggleDropdown = () => {
+  isDropdownVisible.value = !isDropdownVisible.value;
+};
+</script>
 <style>
 body {
   width: 960px;
@@ -52,14 +72,8 @@ body {
   margin-left: 20px;
   color: gray;
 }
-.cal {
-  font-variation-settings: 'FILL' 1, 'wght' 400, 'GRAD' 0, 'opsz' 24;
-  font-size: 30px;
-  color: gray;
-  margin-inline-end: auto;
-}
 
-input[type='date'] {
+input[type='month'] {
   position: relative;
   min-height: 30px;
   background: url(../assets/calendar.svg) no-repeat right 10px center / 35px
@@ -67,18 +81,57 @@ input[type='date'] {
   padding: 10px 12px;
   border: 1px solid white;
   font-size: 20px;
+  font-weight: 700;
 }
 
-input[type='date']::-webkit-clear-button,
-input[type='date']::-webkit-inner-spin-button {
+input[type='month']::-webkit-clear-button,
+input[type='month']::-webkit-inner-spin-button {
   display: none;
 }
-input[type='date']::-webkit-calendar-picker-indicator {
+
+input[type='month']::-webkit-calendar-picker-indicator {
   background: transparent;
   z-index: 1;
 }
 
-input[type='date']::before {
-  content: attr(placeholder);
+.profile-wrapper {
+  position: relative;
+  cursor: pointer;
+}
+
+.dropdown {
+  position: absolute;
+  top: 100%;
+  background-color: white;
+  border: 1px solid #ccc;
+  border-radius: 15px;
+  box-shadow: 0px 10px 20px rgba(0, 0, 0, 0.1);
+  z-index: 1000;
+}
+
+.dropdown ul {
+  list-style: none;
+  margin: 0;
+  padding: 0;
+}
+
+.dropdown li {
+  padding: 10px 20px;
+  cursor: pointer;
+}
+
+.dropdown li:hover {
+  background-color: #f0f0f0;
+}
+
+.set {
+  font-variation-settings: 'FILL' 1, 'wght' 400, 'GRAD' 0, 'opsz' 30;
+  font-size: 22px;
+  color: gray;
+}
+.dropdown-name {
+  margin: 0 auto;
+  font-size: 22px;
+  color: gray;
 }
 </style>
