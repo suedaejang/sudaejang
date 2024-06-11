51889283ba94b9e62d79835aa0a5c9d48137424d
length <dlagudrb78@gmail.com>
2024-06-11 16:28:53 +0900

    중간 커밋

---
+++ skeletone-app/src/pages/Home.vue
@@ -0,0 +1,17 @@
+<template>
+  <div class="card card-body">
+    <!-- <a @click="router.push(`/edit2/${todoItem.id}`)">편집</a> -->
+    <a @click="router.push(`/edit2/user1`)">편집</a>
+  </div>
+</template>
+
+<script setup>
+import { useRouter } from 'vue-router';
+import { inject } from 'vue';
+
+defineProps({
+  todoItem: { Type: Object, required: true },
+});
+
+const router = useRouter();
+</script>
--- skeletone-app/src/router/index.js
+++ skeletone-app/src/router/index.js
@@ -1,5 +1,6 @@
-import { createRouter, createWebHistory } from 'vue-router'
-import HomeView from '../views/HomeView.vue'
+import { createRouter, createWebHistory } from 'vue-router';
+import Home from '../pages/Home.vue';
+import EditChart_2 from '../pages/editChart_2.vue';
 
 const router = createRouter({
   history: createWebHistory(import.meta.env.BASE_URL),
@@ -7,17 +8,17 @@ const router = createRouter({
     {
       path: '/',
       name: 'home',
-      component: HomeView
+      component: Home,
     },
     {
-      path: '/about',
-      name: 'about',
-      // route level code-splitting
-      // this generates a separate chunk (About.[hash].js) for this route
-      // which is lazy-loaded when the route is visited.
-      component: () => import('../views/AboutView.vue')
-    }
-  ]
-})
+      path: '/edit/',
+      component: EditChart_2,
+    },
+    {
+      path: '/edit2/:id',
+      component: EditChart_2,
+    },
+  ],
+});
 
-export default router
+export default router;
